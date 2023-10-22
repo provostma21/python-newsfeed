9707518831ffe4ca7fceabb76d2c193c9468abe1
Matthew M. Provost <provostmat@gmail.com>
2023-10-21 21:20:45 -0400

    received error related to vote_count, resolved by removing brackets around func.count(Vote.id)

--- app/models/Post.py
+++ app/models/Post.py
@@ -12,10 +12,14 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
   vote_count = column_property(
-    select([func.count(Vote.id)]).where(Vote.post_id == id)
+    select(func.count(Vote.id)).where(Vote.post_id == id)
   )
+  
   user = relationship('User')
   comments = relationship('Comment', cascade='all,delete')
   votes = relationship('Vote', cascade='all,delete')
+  
+
   
