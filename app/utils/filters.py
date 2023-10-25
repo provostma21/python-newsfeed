b8815ef34f0dbe2c2ce985341a8e84acb1165922
Matthew M. Provost <provostmat@gmail.com>
2023-10-24 21:36:26 -0400

    added function to return date in app/utils/filters

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,2 +1,13 @@
+from datetime import datetime
+
+
 def format_date(date):
   return date.strftime('%m/%d/%y')
+
+print(format_date(datetime.now()))
+
+def format_url(url):
+  return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+print(format_url('http://google.com/test/'))
+print(format_url('https://www.google.com?q=test'))
