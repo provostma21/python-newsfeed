13d3e1de4a8dc291becf1a0ce29af492e59de45f
Matthew M. Provost <provostmat@gmail.com>
2023-10-26 21:17:54 -0400

    updated signup() in routes/api so a new user is created with the input data and saved to the database

--- app/routes/api.py
+++ app/routes/api.py
@@ -7,6 +7,17 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
-  print(data)
+  db = get_db()
+
+  # create a new user
+  newUser = User(
+    username = data['username'],
+    email = data['email'],
+    password = data['password']
+  )
+
+  # save in database
+  db.add(newUser)
+  db.commit()
 
   return ''
