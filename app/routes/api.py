a057945cd61c2eb54dc1dc274b36c30229465122
Matthew M. Provost <provostmat@gmail.com>
2023-10-27 22:41:45 -0400

    added /comments route and post method with error catch

--- app/routes/api.py
+++ app/routes/api.py
@@ -55,3 +55,24 @@ def login():
   session['loggedIn'] = True
 
   return jsonify(id = user.id)
+
+@bp.route('/comments', methods=['POST'])
+def comment():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+  # create a new comment
+  newComment = Comment(
+    comment_text = data['comment_text'],
+    post_id = data['post_id'],
+    user_id = session.get('user_id')
+  )
+
+  db.add(newComment)
+  db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Comment failed'), 500
