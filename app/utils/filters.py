974afb6123409cc21b5c3fb6082a5b814f527b5f
Matthew M. Provost <provostmat@gmail.com>
2023-11-15 22:17:27 -0500

    added comments to app/utils/filters

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 
-
+# Sets the date format
 def format_date(date):
   return date.strftime('%m/%d/%y')
 
 def format_url(url):
   return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
 
+# Adds plural filter to make a word plural if there are multiple of them
 def format_plural(amount, word):
   if amount != 1:
     return word + 's'
